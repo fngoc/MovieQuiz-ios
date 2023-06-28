6472c792f9b4c835348c7aee142f0183484d201e
VitMarKha <vitmarkha@icloud.com>
2023-06-28 15:30:15 +0300

    add logic chance questions

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Виталий Хайдаров on 28.06.2023.
+//
+
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
