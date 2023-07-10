3bcc40bb30d4f9760ba06599f3bfc31df60baecd
VitMarKha <vitmarkha@icloud.com>
2023-07-10 18:40:27 +0300

    weak reference

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -52,7 +52,9 @@ final class MovieQuizViewController: UIViewController {
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
             self.currentQuestionIndex = 0
             self.correctAnswer = 0
             self.show(quiz: self.convert(model: self.questions[self.currentQuestionIndex]))
@@ -66,7 +68,8 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         correctAnswer = isCorrect ? correctAnswer + 1 : correctAnswer
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.imageView.layer.borderWidth = 0 // Убираю обводку вокруг картинки
             self.showNextQuestionOrResults()
             self.setAvailableButtons()
